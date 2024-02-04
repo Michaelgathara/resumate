ed0896b89036f1b107ecf4c32a893cb167a971a3
Akshar Patel <aksharmpatel1@gmail.com>
2024-02-03 20:06:12 -0600

    pdf reader complete

--- pdf_parser/parse.py
+++ pdf_parser/parse.py
@@ -1,12 +1,30 @@
 # !pip install pymupdf
 
 import fitz
+import re
 
-fpath = "../resumes/resume_0/resume_0.pdf"
 
-doc = fitz.open(fpath)
-text = ""
-for page in doc:
-    text += page.get_text()
+# rnum = 18
+# fpath = f"../resumes/resume_{rnum}/resume_{rnum}.pdf"
 
-print(text)
+# doc = fitz.open(fpath)
+# text = ""
+# # toks = []
+# for page in doc:
+#     text += re.sub(r"/\d\.\s+|[a-z]\)\s+|•\s+|[A-Z]\.\s+|[IVX]+\.\s+/g", "", page.get_text())
+#     # toks.append(page.get_text("words", delimiters=None))
+
+# print(rnum, rnum, rnum, rnum, rnum)
+# print(text)
+
+
+for rnum in range(20):
+    fpath = f"../resumes/resume_{rnum}/resume_{rnum}.pdf"
+
+    doc = fitz.open(fpath)
+    text = ""
+    for page in doc:
+        # text += re.sub(r"/\d\.\s+|[a-z]\)\s+|•\s+|[A-Z]\.\s+|[IVX]+\.\s+/g", "", page.get_text())
+        text += re.sub(r"●( |\n)|\||-( |\n)|▪( |\n)|•( |\n)|‑( |\n)", "", page.get_text())
+    print("RESUME NUMBER", rnum)
+    print(text)
